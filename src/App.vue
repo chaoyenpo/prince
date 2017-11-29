d8f1cdae9239c3f21e7f9e44c442b8dc1fccb3b6
chaoyenpo <chao.yen.po@gmail.com>
2017-11-29 12:42:28 +0800

    add animate

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,8 @@ export default {
   @import '~sweetalert2/dist/sweetalert2.css';
 </style>
 <style lang="scss">
+  /* Import Animate Styles */
+  @import '~animate.css';
   // Import Main styles for this application
   @import './scss/style';
 </style>
