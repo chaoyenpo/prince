b366f21b3fee43aa580b9353f81031e05447de8f
chaoyenpo <chao.yen.po@gmail.com>
2017-12-03 01:52:44 +0800

    no message

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <footer class="footer-bg"> -->
+  <footer>
+    <div class="container-fluid bg-light py-2 mt-3">
+      <div class="d-flex align-items-center align-items-sm-start">
+        <div class="d-flex flex-column flex-sm-row justify-content-between align-items-center w-100">
+          <div class="ml-auto d-flex flex-column flex-md-row justify-content-between">
+            © 2017 XXX Inc.
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'footer',
+};
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Footer from './Footer';
 
 export {
   Navbar,
   Header,
   Sidebar,
+  Footer,
 };
